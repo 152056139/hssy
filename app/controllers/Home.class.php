5ed3139b6f94e9b51813dece51cf7686c1d79d1f
Gray-Young <1162032953@qq.com>
2017-09-14 17:47:22 +0000

    zaicigengzheng

--- app/controllers/Home.class.php
+++ app/controllers/Home.class.php
@@ -1,9 +1,9 @@
 <?php
-<<<<<<< HEAD
   class Home extends Controller
   {
     public function index($data = array())
     {
+      echo "这里是home控制器里的index方法";
       //加载首页页面
       $this->show('index/index', $data);
     }
